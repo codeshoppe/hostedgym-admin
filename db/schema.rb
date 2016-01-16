d9c58ec729901b53d59e4c5704c58ea1f88d857d
Carleton DiLeo <cbdileo@gmail.com>
2016-01-16 01:02:30 -0800

    Remove posted_on from Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116062220) do
+ActiveRecord::Schema.define(version: 20160116090041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.datetime "posted_on"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/factories/posts_factory.rb
+++ spec/factories/posts_factory.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :post do
     title "Title 1"
-    posted_on 1.years.ago
     body "This is the body"
   end
 end
