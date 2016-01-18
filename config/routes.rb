12ffc76bf632b2ab2be0e47460c5ec2b40a53743
Jennifer Reyes <jgdreyes@gmail.com>
2016-01-17 22:09:17 -0800

    Starting to add payments but WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "articles#index"
   resources :articles
 
   post '/contact_messages', to: 'contact_messages#create'
+  get 'payments/index'
+  post 'payments/checkout'
+
 end
