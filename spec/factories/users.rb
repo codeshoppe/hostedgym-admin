297c87b957f26db6232ba6f59c8549ef0e398051
Jennifer Reyes <jgdreyes@gmail.com>
2016-01-22 10:09:47 -0800

    Adding more model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,25 @@
 FactoryGirl.define do
   factory :user do
-    
-  end
+    sequence :email do |n|
+      "email_#{n}@email.com"
+    end
 
+    sequence :first_name do |n|
+      "first_name_#{n}"
+    end
+
+    sequence :last_name do |n|
+      "last_name_#{n}"
+    end
+
+    password 'fakepassword&*'
+
+    factory :admin do
+      admin true
+    end
+
+    factory :user_with_braintree_customer do
+      association :braintree_customer, strategy: :build
+    end
+  end
 end
