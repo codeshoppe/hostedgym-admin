fb43c926db6f33bb03ee7189b241a62b1adc6b49
Jennifer Reyes <jgdreyes@gmail.com>
2016-01-22 09:35:42 -0800

    Removing unsed files

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,9 @@ Rails.application.routes.draw do
   resource :membership, only: [:new, :create, :show]
   get 'join_now', to: 'memberships#new', as: :join_now
 
-
   devise_for :users
 
   root "dashboard#index"
 
   post '/contact_messages', to: 'contact_messages#create'
-  get 'payments/index'
-  post 'payments/checkout'
-
 end
