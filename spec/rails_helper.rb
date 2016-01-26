66980b9622371b0952480ddad3a7eeb4860a4142
Jennifer Reyes <jgdreyes@gmail.com>
2016-01-25 20:29:59 -0800

    Specify requires for support

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,10 @@ FakeBraintree.activate!
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+require Rails.root.join('spec/support/controller_macros.rb')
+require Rails.root.join('spec/support/devise.rb')
+require Rails.root.join('spec/support/fake_braintree.rb')
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
