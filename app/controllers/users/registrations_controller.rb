0324b8f51c60340c67d629f12a25c7a73f9cdb2a
Carleton DiLeo <cbdileo@gmail.com>
2016-01-29 10:00:37 -0800

    Removed duplicate braintree account creation
    * Removed after_create from user which would always create a braintree
    account
    * Consolidated the braintree method on the registration controller to
    * remove duplicate logic

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,25 +1,21 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  after_filter :add_account
+  after_filter :sync_to_payment_service
 
   protected
 
-  def add_account
+  def sync_to_payment_service
     if resource.persisted?
       braintree_id = PaymentService::Vault.find_customer_id(resource.email)
-      if braintree_id.present?
-        resource.create_braintree_customer(customer_id: braintree_id)
-      else
-        sync_to_payment_service(resource)
-      end
-    end
-  end
 
-  def sync_to_payment_service(resource)
-    customer_id = PaymentService::Vault.store_customer(resource.email, resource.first_name, resource.last_name)
-    if customer_id.present?
-      resource.create_braintree_customer!(customer_id: customer_id)
-    else
-      raise Exception, 'Could not sync user to payment service'
+      if braintree_id.blank?
+        braintree_id = PaymentService::Vault.store_customer(resource.email, resource.first_name, resource.last_name)
+      end
+
+      if braintree_id.present?
+        resource.create_braintree_customer!(customer_id: braintree_id)
+      else
+        raise Exception, 'Could not sync user to payment service'
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,6 @@ class User < ActiveRecord::Base
   scope :admins, -> { where(admin: true) }
   scope :accounts, -> { where(admin: false) }
 
-  after_create :sync_to_payment_service
-
   def self.policy_class
     AccountPolicy
   end
@@ -28,15 +26,5 @@ class User < ActiveRecord::Base
 
   def can_sign_up?
     !gym_member? && invited?
-  end
-
-  private
-  def sync_to_payment_service
-    customer_id = PaymentService::Vault.store_customer(self.email, self.first_name, self.last_name)
-    if customer_id.present?
-      self.create_braintree_customer!(customer_id: customer_id) 
-    else
-      raise Exception, 'Could not sync user to payment service'
-    end
   end
 end
